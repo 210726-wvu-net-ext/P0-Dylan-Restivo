e55faa924754bc8609f047798bdc4e7d4f6fbb8a
mmookow <dylan.restivo@yahoo.com>
2021-08-14 18:43:51 -0500

    Fleshed out classes more and started program intro. Attmepted Sqlite and file writing

--- Project0/App/Main.cs
+++ Project0/App/Main.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using Lib;
+
 
 /// <summary>
 /// - add a new user 
@@ -17,7 +19,21 @@ namespace App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartUpFunc();
+        }
+
+        public static void StartUpFunc(){
+            List<User> users = new List<User>();
+            // List<Restaurant> restaurants = new List<Restaurant>();
+            // List<Review> reviews = new List<Review>();
+            Console.WriteLine("Welcome to EateryEvals!");
+            Console.WriteLine("If you are a user please enter \"user\".\nIf you are an admin please enter \"admin\".");
+            string response = Console.ReadLine();
+            if (response.ToLower() == "user"){
+                Console.WriteLine("Welcome user");
+            }else if (response.ToLower() == "admin"){
+                Console.WriteLine("Welcome admin");
+            }
         }
     }
 }
--- Project0/Lib/User.cs
+++ Project0/Lib/User.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 /// <summary>
 /// - add a new user
 /// - add reviews to a restaurant as a user 
@@ -7,30 +8,31 @@
 /// </summary>
 namespace Lib
 {
-    class User
+    public class User
     {
         // create a user object
         // function for user to lookup their reviews
         // user should have an int id, string name
-        public User(int id, int name)
+        public User(int id, string name)
         {
-            
+            this.Name = name;
+            this.Id = id;
         }
         private int id;
-        public int Id {
-            get { return id; }
-            set { id = value; } 
-            }
+        public int Id { get; set; }
         private string name;
-        public string Name {
-            get { return name; }
-            set { name = value; } 
-            }
+        public string Name { get; set; }
 
+        public void PrintUser(){
+            Console.WriteLine($"This user is {this.Id} and user id is {this.Name}");
+        }
         //Write review function write result to file
         public void WriteReview(){
 
 
+        }
+        public List<User> CreateUser(int id, string name){
+            
         }
 
     }

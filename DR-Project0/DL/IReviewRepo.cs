a4395983a87e87f18871c9b5572c42f485eade11
Dylan Restivo <dylan.restivo@yahoo.com>
2021-08-22 21:57:56 -0500

    Added all functionality. Bugs to fix

---
+++ DR-Project0/DL/IReviewRepo.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Models;
+
+namespace DL
+{
+    public interface IReviewRepo
+    {
+        List<Restaurant> GetAllRestaurants();
+
+        List<Users> GetAllUsers();
+    
+        List<Reviews> GetAllReviews();
+
+        List<Reviews> GetAvgRatings(int id);
+
+        Restaurant GetRestaurantByName(string name);
+        Restaurant GetRestaurantById(int id);
+
+        Restaurant RestaurantLookupZip(string zipcode);
+
+        List<Models.Reviews> GetReviewsByRestaurantId(int restaurantId);
+
+        Reviews AddAReview(Reviews review);
+
+        Users AddAUser(Users AddAUser);
+
+        Restaurant GetRestaurantForAdd(string name);
+
+        Users GetUserPassword(string userName);
+
+        string GetAdminPassword();
+
+        bool GetUserName(string userName);
+
+        int GetUserId(string username);
+    }
+}
---
+++ DR-Project0/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Models
+{
+    public class Users
+    {
+        public Users(){}
+        public Users(string name)
+        {
+            this.Name = name;
+        }
+        public Users(string name, string username, string password) : this(name)
+        {   
+            this.UserName = username;
+            this.Password = password;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
